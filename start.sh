f7b8a7145a77e1a0afdcc39cb515d6380ced094e
Capn Flint <capnflinttv@gmail.com>
2019-09-25 13:46:00 -0700

    sudo work!

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 sudo docker build -t grogsocket .
-sudo docker run --rm -it -p 9001:9001 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $(readlink -f /etc/letsencrypt/live/www.capnflint.com/privkey.pem):certs/privkey.pem:ro -v $(readlink -f /etc/letsencrypt/live/www.capnflint.com/fullchain.pem):certs/fullchain.pem:ro --name grogsocket grogsocket
+sudo docker run --rm -it -p 9001:9001 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $(sudo readlink -f /etc/letsencrypt/live/www.capnflint.com/privkey.pem):certs/privkey.pem:ro -v $(sudo readlink -f /etc/letsencrypt/live/www.capnflint.com/fullchain.pem):certs/fullchain.pem:ro --name grogsocket grogsocket
